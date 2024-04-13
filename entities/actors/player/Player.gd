51a32f596f81c40987b43418e1091f95218da91b
Dmitry <zeddmusicmaster@gmail.com>
2024-04-13 10:37:53 +0300

    Some changes

--- entities/actors/player/Player.gd
+++ entities/actors/player/Player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 200.0
+const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -20,6 +20,8 @@ func _physics_process(delta):
 func applyGravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		if velocity.y > 0:
+			$AnimatedSprite2D.play("Falling")
 	
 	
 func handleJump():
